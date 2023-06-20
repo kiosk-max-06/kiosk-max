2941397f6949a713761919d3b381ce2187b5a106
Daeram Chung <daeram.chung@gmail.com>
2023-06-20 19:22:27 +0900

    #7 design: Panel 컴포넌트 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Tabs from "./components/Tabs/Tabs.tsx";
+import Panel from "./components/Tabs/Panel.tsx";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Tabs />
+      <Panel />
     </div>
   );
 }
