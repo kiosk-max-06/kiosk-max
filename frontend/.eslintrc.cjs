03da6fd900ec4d280a7102e9b60a638801509907
Daeram Chung <daeram.chung@gmail.com>
2023-06-20 18:28:38 +0900

    #6 chore: Prettier, ESLint 충돌 해결

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -2,13 +2,19 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ["airbnb", "plugin:@typescript-eslint/recommended", "prettier"],
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "react-app",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   rules: {
-    "prettier/prettier": ["error"],
+    "prettier/prettier": ["error", { singleQuote: false }],
     "@typescript-eslint/no-non-null-assertion": "off",
     "react/jsx-filename-extension": ["warn", { extensions: [".tsx"] }],
+    "quotes": ["error", "double"],
   },
   root: true,
 };
