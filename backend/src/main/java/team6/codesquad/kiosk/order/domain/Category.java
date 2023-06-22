5e22b10a11d841d20bc887f71ce7323c8a06ab20
sudago <sudago22@gmail.com>
2023-06-22 23:48:54 +0900

    #12 feat: domain에 getter 애노테이션 추가

--- backend/src/main/java/team6/codesquad/kiosk/order/domain/Category.java
+++ backend/src/main/java/team6/codesquad/kiosk/order/domain/Category.java
@@ -1,5 +1,8 @@
 package team6.codesquad.kiosk.order.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Category {
     private int id;
     private String name;
--- backend/src/main/java/team6/codesquad/kiosk/order/domain/Option.java
+++ backend/src/main/java/team6/codesquad/kiosk/order/domain/Option.java
@@ -1,5 +1,8 @@
 package team6.codesquad.kiosk.order.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Option {
     private int id;
     private String name;
