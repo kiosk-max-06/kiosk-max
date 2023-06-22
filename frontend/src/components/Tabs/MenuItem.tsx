5a19983cfe1a37f72dc8649947942dbde6511044
Daeram Chung <daeram.chung@gmail.com>
2023-06-22 10:51:56 +0900

    #7 style: import 순서 수정

--- frontend/src/components/Tabs/MenuItem.tsx
+++ frontend/src/components/Tabs/MenuItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./MenuItem.module.css";
 import { IMenuItemProps } from "../../types/Tabs.ts";
+import styles from "./MenuItem.module.css";
 
 function MenuItem({ name, price }: IMenuItemProps) {
   return (
