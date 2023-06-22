a057b5ad492c763698079f4c44c7e3293c2910b1
sudago <sudago22@gmail.com>
2023-06-22 23:48:16 +0900

    #12 feat: 홈페이지 방문 시, 모든 카테고리 및 메뉴 반환

---
+++ backend/src/main/java/team6/codesquad/kiosk/order/service/MenuService.java
@@ -0,0 +1,22 @@
+package team6.codesquad.kiosk.order.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import team6.codesquad.kiosk.order.dto.response.CategoryResponseDto;
+import team6.codesquad.kiosk.order.repository.MenuRepository;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MenuService {
+	private final MenuRepository menuRepository;
+
+	public List<CategoryResponseDto> getCategories() {
+		return menuRepository.findAll();
+	}
+
+}
