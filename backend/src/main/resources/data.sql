1d02c88bb9881188a1a48f4d9b9d96dc68e71289
sudago <sudago22@gmail.com>
2023-06-23 11:25:12 +0900

    #4 fix: 데이터 접근 불가 한 오류로 스크립트 수정

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,112 +1,47 @@
--- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
---
--- Host: 127.0.0.1    Database: kiosk
--- ------------------------------------------------------
--- Server version	8.0.33
+-- category
+INSERT INTO `category`(name)
+VALUES ('커피'),
+       ('라떼'),
+       ('티'),
+       ('쥬스'),
+       ('디카페인');
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- menu
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('아메리카노', '4000', '이미지', '1');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('카페모카', '4500', '이미지', '1');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('에스프레소', '3000', '이미지', '1');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('콜드브루', '3500', '이미지', '1');
 
---
--- Dumping data for table `category`
---
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('카페라떼', '4000', '이미지', '2');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('바닐라라떼', '4500', '이미지', '2');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('오곡라떼', '3000', '이미지', '2');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('고구마라떼', '3500', '이미지', '2');
 
-LOCK TABLES `category` WRITE;
-/*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'커피'),(2,'라떼'),(3,'티'),(4,'쥬스'),(5,'디카페인');
-/*!40000 ALTER TABLE `category` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('녹차', '4000', '이미지', '3');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('페퍼민트', '4500', '이미지', '3');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('우롱차', '3000', '이미지', '3');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('캐모마일', '3500', '이미지', '3');
 
---
--- Dumping data for table `menu`
---
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('오렌지쥬스', '4000', '이미지', '4');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('포도쥬스', '4500', '이미지', '4');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('키위쥬스', '3000', '이미지', '4');
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('사과쥬스', '3500', '이미지', '4');
 
-LOCK TABLES `menu` WRITE;
-/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,'아메리카노',4000,'이미지',1),(2,'카페모카',4500,'이미지',1),(3,'에스프레소',3000,'이미지',1),(4,'콜드브루',3500,'이미지',1),(5,'카페라떼',4000,'이미지',2),(6,'바닐라라떼',4500,'이미지',2),(7,'오곡라떼',3000,'이미지',2),(8,'고구마라떼',3500,'이미지',2),(9,'녹차',4000,'이미지',3),(10,'페퍼민트',4500,'이미지',3),(11,'우롱차',3000,'이미지',3),(12,'캐모마일',3500,'이미지',3),(13,'오렌지쥬스',4000,'이미지',4),(14,'포도쥬스',4500,'이미지',4),(15,'키위쥬스',3000,'이미지',4),(16,'사과쥬스',3500,'이미지',4),(17,'아메리카노(디카프)',3500,'이미지',5);
-/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `option`
---
-
-LOCK TABLES `option` WRITE;
-/*!40000 ALTER TABLE `option` DISABLE KEYS */;
-INSERT INTO `option` VALUES (1,'큰거'),(2,'작은거'),(3,'뜨거운것');
-/*!40000 ALTER TABLE `option` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `order`
---
-
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `order_menu`
---
-
-LOCK TABLES `order_menu` WRITE;
-/*!40000 ALTER TABLE `order_menu` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_menu` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `order_menu_option`
---
-
-LOCK TABLES `order_menu_option` WRITE;
-/*!40000 ALTER TABLE `order_menu_option` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_menu_option` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `order_status`
---
-
-LOCK TABLES `order_status` WRITE;
-/*!40000 ALTER TABLE `order_status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_status` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `payment`
---
-
-LOCK TABLES `payment` WRITE;
-/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
-INSERT INTO `payment` VALUES (1,'카드결제'),(2,'현금결제');
-/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `sales`
---
-
-LOCK TABLES `sales` WRITE;
-/*!40000 ALTER TABLE `sales` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sales` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-06-22 23:00:40
+INSERT INTO `menu`(name, price, image, category_id)
+VALUES ('아메리카노(디카프)', '3500', '이미지', '5');
