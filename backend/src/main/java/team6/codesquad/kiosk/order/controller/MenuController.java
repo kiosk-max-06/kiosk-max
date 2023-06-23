d330d30885b10f8365665b1eac54bb4960d5ac0c
sudago <sudago22@gmail.com>
2023-06-23 11:13:20 +0900

    #12 fix: 메뉴 반환 시 Object ->  List 로 반환

--- backend/src/main/java/team6/codesquad/kiosk/order/controller/MenuController.java
+++ backend/src/main/java/team6/codesquad/kiosk/order/controller/MenuController.java
@@ -1,10 +1,12 @@
 package team6.codesquad.kiosk.order.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import team6.codesquad.kiosk.order.dto.response.CategoriesResponseDto;
+import team6.codesquad.kiosk.order.dto.response.CategoryResponseDto;
 import team6.codesquad.kiosk.order.service.MenuService;
 
 @RestController
@@ -12,8 +14,8 @@ import team6.codesquad.kiosk.order.service.MenuService;
 public class MenuController {
 	private final MenuService menuService;
 
-	@GetMapping("")
-	public CategoriesResponseDto getMenuList() {
-		return new CategoriesResponseDto(menuService.getCategories());
+	@GetMapping("/")
+	public List<CategoryResponseDto> getMenuList() {
+		return menuService.getCategories();
 	}
 }
