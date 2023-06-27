212e45da0695d08f03f7509cadcc026f7f4e95d5
JJONSOO <pjs970616@naver.com>
2023-06-27 11:30:01 +0900

    #29 chore : Api 요청에 대한 설명 추가

--- backend/src/main/java/team6/codesquad/kiosk/order/controller/OrderController.java
+++ backend/src/main/java/team6/codesquad/kiosk/order/controller/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import team6.codesquad.kiosk.order.controller.dto.OrderRequest;
 import team6.codesquad.kiosk.order.service.OrderService;
@@ -15,6 +16,7 @@ public class OrderController {
 
 	private final OrderService service;
 
+	@Operation(summary = "모든 카테고리 및 메뉴", description = "사용자에게 모든 카테고리 및 메뉴를 알려준다.")
 	@PostMapping("/order")
 	public ResponseEntity<Void> createOrder(@RequestBody OrderRequest orderRequest) {
 		//ResponseEntity<> 감싸서 내보내야 JSON형태로 내보내짐.
