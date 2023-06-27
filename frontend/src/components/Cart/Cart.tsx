3f5549a50da0ac1edab7fea30d74d8953142403e
Daeram Chung <daeram.chung@gmail.com>
2023-06-27 22:16:24 +0900

    #32 refactor: Cart timeout 초기화 조건 수정

--- frontend/src/components/Cart/Cart.tsx
+++ frontend/src/components/Cart/Cart.tsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect, FormEvent } from "react";
-import { IController } from "../../types/Modal.ts";
+import React, { useState, useEffect, useRef, FormEvent } from "react";
+import { IController, IMenu } from "../../types/Modal.ts";
 import styles from "./Cart.module.css";
 import { EActiveModal } from "../../constants/Modal.ts";
 
 function Cart({ ctrl }: { ctrl: IController }) {
-  const [remTime, setRemTime] = useState(30);
+  const [remTime, setRemTime] = useState(300);
+  const cart = ctrl.cart.get();
+
+  const prevNumItems = useRef<number>(cart ? cart.menuList.length : 0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -12,22 +15,21 @@ function Cart({ ctrl }: { ctrl: IController }) {
       if (remTime === 1) {
         ctrl.cancel();
       }
-    }, 1000);
+    }, 100);
 
     return () => {
       clearInterval(intervalId);
-      if (!ctrl.cart.get()) {
-        setRemTime(30);
+      if (!cart || prevNumItems.current !== cart.menuList.length) {
+        prevNumItems.current = cart ? cart.menuList.length : 0;
+        setRemTime(300);
       }
     };
-  }, [ctrl, remTime]);
-
-  const cart = ctrl.cart.get();
+  }, [ctrl, cart, remTime]);
 
   function formSubmitHandler(e: FormEvent) {
     e.preventDefault();
     if (!cart) return;
-    const { menuList } = cart;
+    const { menuList } = cart!;
     ctrl.order.set({ menuList });
     ctrl.cart.clear();
     ctrl.modal.view(EActiveModal.PAYMENT);
@@ -45,7 +47,7 @@ function Cart({ ctrl }: { ctrl: IController }) {
             type="hidden"
             id="menuList"
             name="menuList"
-            value={JSON.stringify(cart.menuList)}
+            value={JSON.stringify(cart!.menuList)}
           />
         )}
 
@@ -53,16 +55,7 @@ function Cart({ ctrl }: { ctrl: IController }) {
           <div className={styles.cart__left}>
             {cart && (
               <ul className={styles.cart__ul}>
-                {cart.menuList.map((menu, i) => {
-                  const key = Symbol(i);
-                  const { data } = menu;
-                  const { name, price, imgUrl } = data;
-                  return (
-                    <li key={key.toString()} className={styles.cart__li}>
-                      <img src={imgUrl} alt="" />
-                    </li>
-                  );
-                })}
+                {cart!.menuList.map(CartItem)}
               </ul>
             )}
           </div>
@@ -76,11 +69,33 @@ function Cart({ ctrl }: { ctrl: IController }) {
             <button className={styles.cart__button} type="submit">
               결제하기
             </button>
-            <p className="timer">{remTime}초 남음</p>
+            <p className="timer">{(remTime / 10).toFixed()}초 남음</p>
           </div>
         </div>
       </form>
     </article>
+  );
+}
+
+function CartItem(menu: IMenu, i: number) {
+  const key = Symbol(i);
+  const { data, count } = menu;
+  const { name, price, imgUrl } = data;
+
+  return (
+    <li key={key.toString()} className={styles.cart__li} data-count={count}>
+      <figure>
+        <img src={imgUrl} alt="" />
+        <figcaption className="blind">{name}</figcaption>
+      </figure>
+      <dl>
+        <dt className="blind">이름:</dt>
+        <dd>{name}</dd>
+
+        <dt className="blind">가격:</dt>
+        <dd>{price}</dd>
+      </dl>
+    </li>
   );
 }
 
