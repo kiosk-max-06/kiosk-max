e1ca074618bc5b2772ab700af6d22dd279f88578
JJONSOO <pjs970616@naver.com>
2023-06-30 11:20:07 +0900

    #45 chore : ec2 port 3000 열어두도록 수정

--- backend/src/main/java/team6/codesquad/kiosk/config/CorsConfiguration.java
+++ backend/src/main/java/team6/codesquad/kiosk/config/CorsConfiguration.java
@@ -11,7 +11,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins(
-				"http://localhost:3000")
+				"http://localhost:3000",
+				"http://13.125.16.208:3000")
 			.allowedMethods("GET", "POST", "OPTION", "PUT");
 	}
 }
